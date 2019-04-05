e3627e6c804f5a1fd617c19a6fa3d05b19c62383
Emma Gouti <gouti.naama@gmail.com>
2019-04-04 21:36:03 -0500

    variables

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -13,7 +13,18 @@ print (f)
 
 # # ERROR: variables of different types cannot be combined
 print ("string type " + str(123))
-# str convert number to string
+    # str convert number to string
 
 # Global vs. local variables in functions
+def someFunction():
+  #global f
+    #tells the function that f is now global, outputs def, def twice   
+  f = "def"
+  print (f)
 
+someFunction()
+print (f) 
+
+# del f 
+#  del undefines a variable in real time
+# print(f)
