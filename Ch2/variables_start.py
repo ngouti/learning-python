4d6783979e318ca64311d5b142f7f81781808f07
Emma Gouti <gouti.naama@gmail.com>
2019-04-04 21:32:20 -0500

    variable

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -3,13 +3,17 @@
 #
 
 # Declare a variable and initialize it
-
+f = 0 
+print(f)
 
 # # re-declaring the variable works
+f = "abc"
+print (f)
 
 
 # # ERROR: variables of different types cannot be combined
-
+print ("string type " + str(123))
+# str convert number to string
 
 # Global vs. local variables in functions
 
