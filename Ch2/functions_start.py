8ee960a8bbafc2f8e8096616cec2fc9af7c9a531
Emma Gouti <gouti.naama@gmail.com>
2019-04-07 15:30:17 -0500

    functions

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -8,15 +8,27 @@ def func1():
     # python uses scope definer (colon) & indentation
 
 # function that takes arguments
-
+def func2(arg1, arg2):
+    print(arg1, " ", arg2)
 
 # function that returns a value
-
+def cube(x):
+    return x*x*x
 
 # function with default value for an argument
-
+def power(num, x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
 
 #function with variable number of arguments
+def multi_add(*args):
+    result = 0
+    for z in args:
+        result = result + z
+    return result
+
 
 func1() 
 # function being called directly
@@ -28,3 +40,8 @@ print (func1())
 print (func1)
 # were just print the function value which is an object
 # <function func1 at 0x0477B390>
+
+func2(10,20)
+print (func2(10,20))
+print (cube(3))
+print (power(2))
