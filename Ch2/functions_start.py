202594c1882e317fd5a56a70b0dceccc796627bf
Emma Gouti <gouti.naama@gmail.com>
2019-04-07 15:10:13 -0500

    functions

--- Ch2/functions_start.py
+++ Ch2/functions_start.py
@@ -3,7 +3,9 @@
 #
 
 # define a basic function
-
+def func1():
+    print("I am a function")
+    # python uses scope definer (colon) & indentation
 
 # function that takes arguments
 
@@ -16,3 +18,13 @@
 
 #function with variable number of arguments
 
+func1() 
+# function being called directly
+print (func1())
+# function being called in print function, then outer print fX executes
+# our function doesnt return value so Python evalutes return value to be None 
+# I am a function
+# None
+print (func1)
+# were just print the function value which is an object
+# <function func1 at 0x0477B390>
